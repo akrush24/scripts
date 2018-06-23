b07fcc6f41d341ce50f3be0c1cda54d9397c74fa
Andrey <root@orangepiplus>
2018-06-24 00:43:58 +0300

    From orangepiplus, 2018.06.24 00:43:58

--- cloud.mail/start.sh
+++ cloud.mail/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/akrush/scripts/CloudMail
+cd /home/akrush/scripts/cloud.mail/CloudMail
 nohup sudo mono wdmrc.exe -p 801 &
 sleep 10
 sudo mount --rw -t davfs http://127.0.0.1:801 /mnt/cloud.mail.davfs/
